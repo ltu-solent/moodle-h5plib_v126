e07f5a78b557ce44875ef7485924d0954b2f8e3c
Mark Sharp <mark.sharp@solent.ac.uk>
2024-09-02 09:39:39 +0100

    Requires M4.1 depends on MDL-82967

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023042400;       // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2023041800;       // Requires this Moodle version.
+$plugin->requires  = 2022112800;       // Requires this Moodle version.
 $plugin->component = 'h5plib_v126';    // Full name of the plugin (used for diagnostics).
